738ad067941b6356b019154fa1a3c2df7ef75058
Alec Barber <alecbarber23@gmail.com>
2023-02-06 10:32:13 -0500

    removed some logs, and updated like button to cl when clicked

--- src/App.js
+++ src/App.js
@@ -34,6 +34,10 @@ const App = () => {
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
      */
+
+    const likeCount = posts[postId - 1].likes
+    console.log(likeCount)
+
   };
 
   return (
